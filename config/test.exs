bed6b2b3ac8cce571afc35a39d953919a0d0b2d7
Bryan <hi@braynm.com>
2022-08-02 22:43:52 +0800

    fix correct credentials for database params

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ex_potify, ExPotify.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "bryan",
+  password: "",
   hostname: "localhost",
   database: "ex_potify_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
