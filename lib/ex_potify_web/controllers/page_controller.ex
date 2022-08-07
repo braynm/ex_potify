ddf20defd21e7d47c05a7224b56658b85d08637b
Bryan <hi@braynm.com>
2022-08-07 19:03:35 +0800

    add actions to page_controller

--- lib/ex_potify_web/controllers/page_controller.ex
+++ lib/ex_potify_web/controllers/page_controller.ex
@@ -1,7 +1,18 @@
 defmodule ExPotifyWeb.PageController do
   use ExPotifyWeb, :controller
 
+  # plug :put_layout, false when action in [:home]
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    oauth_url = Spotify.Authorization.url()
+    render(conn, "index.html", oauth_url: oauth_url)
+  end
+
+  # conn
+  def home(conn, _) do
+    conn
+    |> put_layout(false)
+    |> put_root_layout(false)
+    |> render("home.html")
   end
 end
