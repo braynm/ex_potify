758eb21710d561946e1182d21d398a454d44e958
Bryan <hi@braynm.com>
2022-08-18 11:19:31 +0800

    refactor home html template

--- lib/ex_potify_web/controllers/page_controller.ex
+++ lib/ex_potify_web/controllers/page_controller.ex
@@ -5,14 +5,17 @@ defmodule ExPotifyWeb.PageController do
 
   def index(conn, _params) do
     oauth_url = Spotify.Authorization.url()
-    render(conn, "index.html", oauth_url: oauth_url)
+
+    conn
+    |> put_layout(false)
+    |> render("index.html", oauth_url: oauth_url)
   end
 
   # conn
   def home(conn, _) do
+    %{"id" => session_id} = get_session(conn)
+
     conn
-    |> put_layout(false)
-    |> put_root_layout(false)
-    |> render("home.html")
+    |> render("home.html", session_id: session_id)
   end
 end
