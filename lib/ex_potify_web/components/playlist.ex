e85a3f4086eebea17db7e01db7c296d8d9b48343
Bryan <hi@braynm.com>
2022-08-21 21:33:43 +0800

    playlist component changes

--- lib/ex_potify_web/components/playlist.ex
+++ lib/ex_potify_web/components/playlist.ex
@@ -1,14 +1,12 @@
 defmodule ExPotifyWeb.Playlist do
-  use Phoenix.Component
   use Phoenix.LiveView
+  use Phoenix.Component
 
   def index(assigns) do
     IO.inspect(assigns)
     # conn = Spotify.Cookies.set_access_cookie(%{}, access_token)
 
     # playlists = Spotify.Playlist.get_current_user_playlists(conn, limit: 10)
-
-    # IO.inspect(playlists)
 
     ~H"""
      <div class="playlists pt-[24px] pl-[24px]">
@@ -19,12 +17,9 @@ defmodule ExPotifyWeb.Playlist do
        <ul class="overflow-y-scroll max-h-[330px]">
          <%= for item <- @playlists do %>
            <li class="text-[#F2F6FF] pb-[6px] text-sm">
-             <span class="cursor-pointer opacity-100"><%= item.name %></span>
+             <span class="cursor-pointer opacity-75"><%= item.name %></span>
            </li>
          <% end %>
-
-         <a href="#">Load More</a>
-
           <!--
 
          <li class="text-[#F2F6FF] pb-[4px] text-sm">
