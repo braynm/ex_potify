d3f6e6eb144c3982c8545d08992910336f7740a2
Bryan <hi@braynm.com>
2022-08-28 12:11:37 +0800

    request track on select of playlist item

--- lib/ex_potify_web/live/playlist_info_live.ex
+++ lib/ex_potify_web/live/playlist_info_live.ex
@@ -8,27 +8,57 @@ defmodule ExPotifyWeb.PlaylistInfoLive do
       |> assign(:name, "")
       |> assign(:description, "")
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [tracks: []]}
   end
 
   def handle_event("select_playlist", payload, socket) do
-    %{"name" => name, "description" => description} = payload
+    %{
+      "name" => name,
+      "description" => description,
+      "owner_id" => owner_id,
+      "playlist_id" => playlist_id,
+      "session_id" => session_id
+    } = payload
+
+    {:ok, resp} =
+      ExPotify.Playlist.get_playlist_details(
+        session_id,
+        owner_id,
+        playlist_id
+      )
 
     socket =
       socket
       |> assign(:name, name)
+      |> assign(:name, name)
       |> assign(:description, description)
+      |> assign(:tracks, transform_items(resp.items))
 
     {:noreply, socket}
   end
 
+  defp transform_items(items) do
+    Enum.map(items, &do_transform_items/1)
+  end
+
+  defp do_transform_items(%Spotify.Playlist.Track{track: track}) do
+    track_map = Map.from_struct(track)
+
+    %{
+      name: get_in(track_map, [:name]),
+      album: get_in(track_map, [:album, "name"]),
+      artist:
+        track_map
+        |> get_in([:artists])
+        |> List.first()
+        |> Map.get("name")
+    }
+  end
+
   def render(assigns) do
     ~H"""
-    <div id="playlist-info" class="flex pt-[32px] pl-[16px] rounded-tr-[30px] rounded-br-[30px] h-full bg-[#1A2151] w-100 grow">
+    <div id="playlist-info" class="pt-[32px] pl-[16px] rounded-tr-[30px] rounded-br-[30px] h-full bg-[#1A2151] min-w-[760px] max-w-[760px] overflow-y-scroll">
       <div class="playlist-header flex flex-row max-h-[240px]">
-        <div class="playlist-cover mr-[32px]">
-          <%#<img class="rounded-[10px]" width="232" height="232" src="../images/playlist-cover.png"/>%>
-        </div>
         <div class="playlist-description flex flex-col justify-center font-[roboto]">
           <div class="mb-[32px]">
             <span class="text-[16px] text-[#F2F6FF] opacity-[.70]">Playlist</span>
@@ -44,7 +74,30 @@ defmodule ExPotifyWeb.PlaylistInfoLive do
           </div>
         </div>
       </div>
-      <div class="playlist-tracks">
+      <div class="playlist-tracks mt-[48px]">
+        <table class="font-[roboto]">
+          <thead>
+            <tr>
+              <th class="overflow-hidden text-ellipsis whitespace-nowrap font-small text-left text-[#F2F6FF] opacity-[0.7]">Track</th>
+              <th class="font-small text-left text-[#F2F6FF] opacity-[0.7]">Album</th>
+              <th class="font-small text-right text-[#F2F6FF] opacity-[0.7]">Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            <%= for track <- @tracks do %>
+            <tr>
+              <td class="max-w-[350px] w-[350px] text-[#F2F6FF]">
+                <p alt="123" class="whitespace-nowrap overflow-hidden text-ellipsis font-small nowrap "><%= track.name %></p>
+                <p class="pb-[8px] opacity-[0.7] text-sm"><%= track.artist %></p>
+              </td>
+              <td class="max-w-[300px] w-[300px] whitespace-nowrap overflow-hidden text-ellipsis font-small text-[#F2F6FF] nowrap "><%= track.album %></td>
+              <td class="max-w-[10px] w-[10px] text-[#F2F6FF] text-right">3:31</td>
+            </tr>
+            <% end %>
+          </tbody>
+        </table>
+        <div class="min-h-[120px]">
+        </div>
       </div>
     </div>
     """
