5ef5ed82ff810cf5d7ca7149d14f0cf2b243f356
Bryan <hi@braynm.com>
2022-08-29 23:50:20 +0800

    wip — integrate spotify web playback sdk

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
 //import "../css/app.css"
-
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
+
 // to get started and then uncomment the line below.
 // import "./user_socket.js"
 
@@ -47,15 +47,70 @@ import "core-js/features/url"
 
 import topbar from "../vendor/topbar"
 
+window.spotifyReady = false
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let isAllLoaded = false
 let hooks = {
+  Playback2312: {
+    deviceId: null,
+    player: null,
+    initializePlayer() {
+      this.player = new Spotify.Player({
+        name: "ExPotify",
+        getOAuthToken: (cb) => {
+          cb(this.token)
+        }
+      })
+
+      window.addEventListener("keyup", (event) => {
+        switch (event.keyCode) {
+          case 32:
+            this.player.togglePlay()
+          default:
+            console.log("something else..")
+        }
+      })
+
+      this.player.addListener("ready", ({ device_id }) => {
+        this.deviceId = device_id
+        this.pushEvent("init_device", { device_id })
+      })
+
+      this.player.addListener('initialization_error', ({ message }) => {
+        console.error(message);
+      })
+
+      this.player.addListener('authentication_error', ({ message }) => {
+        console.error(message);
+      })
+
+      this.player.addListener('account_error', ({ message }) => {
+        console.error(message);
+      })
+
+      this.player.addListener('playback_error', ({ message }) => {
+        console.error(message);
+      })
+    },
+    mounted() {
+      this.handleEvent("init_token", ({ token }) => {
+        this.token = token
+        if (window.spotifyReady) {
+            console.log('222313512316')
+            this.initializePlayer()
+            this.player.connect()
+        }
+      })
+    },
+    updated() {
+
+    }
+  },
   Scroll: {
     mounted() {
       this.observer = new IntersectionObserver(
         ([entry]) => {
           if (entry.isIntersecting && isAllLoaded === false) {
-            console.log('testing....')
             this.pushEvent("load_more", {})
           }
         },
@@ -66,12 +121,13 @@ let hooks = {
       this.handleEvent("all_loaded", data => {
         isAllLoaded = true
       })
+    },
+    beforeDestroy() {
+      this.observer.unobserve(this.el)
     }
   },
-  beforeDestroy() {
-    this.observer.unobserve(this.el)
-  }
 }
+
 let liveSocket = new LiveSocket("/live", Socket, {hooks, params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
@@ -88,7 +144,6 @@ window.addEventListener("phx:page-loading-start", () => {
 window.addEventListener("phx:page-loading-stop", () => {
   clearTimeout(topBarScheduled)
   topBarScheduled = undefined
-  console.log('hey..asdsadasd')
   topbar.hide()
 })
 
@@ -101,83 +156,6 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = 'BQCh27vuG3MFcnQyAuBBxL0iqBb2gKrybi6_dEYdoVASE_n-h7s6nRUR3GN1rKnsKSMAciDPY8x6mmKuEUJDEYGZ7EUsZt52M85nymSH1365GW9f-p1Rwq8pg5gO9m9q3j8KW7FRekUIgOFXZ736e8ya7F2Id6rvtFjdGXyuFcgBDpDcOebyPy3E3_7LvlYc8Gu9XTEu58ceJXShQDaxdisIuN_RnDKJu0slreby2QaRh7S9nb9SV9Avm-KzpWrhE_LRLNr0Zg6S_JO_i-UeG7l3kTeKbrU-SL_a5lxB';
-  const player = new Spotify.Player({
-    name: 'ExPotify',
-    getOAuthToken: cb => {
-     cb(token);
-    },
-    volume: 1
-  });
-
-
-  let togglePlay = false
-  player.addListener('initialization_error', ({ message }) => {
-    console.error(message);
-  });
-
-  player.addListener('authentication_error', ({ message }) => {
-    console.error(message);
-  });
-
-  player.addListener('account_error', ({ message }) => {
-    console.error(message);
-  });
-
-  player.addListener('playback_error', ({ message }) => {
-    console.error(message);
-  });
-
-  function millisToMinutesAndSeconds(millis) {
-    const minutes = Math.floor(millis / 60000);
-    const seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-  }
-
-  const track = document.getElementById('track')
-  const currentDuration = document.getElementById('currentDuration')
-  const totalDuration = document.getElementById('duration')
-  let progressInPercentage = 0
-  let progress = 0
-  let timing = setInterval(() => {}, 1000)
-  player.addListener('player_state_changed', ({
-   position,
-    duration,
-    track_window: { current_track },
-    paused
-  }) => {
-    console.log('Currently Playing', current_track);
-    console.log('Position in Song', position);
-    console.log('Duration of Song', duration);
-    totalDuration.textContent = millisToMinutesAndSeconds(duration)
-
-    track.max = duration
-    progressPerSec = duration / 1000
-
-    clearInterval(timing)
-
-    if (paused) {
-      togglePlay = false
-    } else {
-      timing  = setInterval(() => {
-        track.value = position !== 0 ? position : progressPerSec
-        console.log({ position, duration, currentPos: progressPerSec })
-        currentDuration.textContent = millisToMinutesAndSeconds(progressPerSec)
-
-        progressPerSec = position !== 0 ? position + 1000 : progressPerSec + 1000
-        position = 0
-
-      }, 1000)
-      player.resume()
-    }
-  });
-
-
-  player.connect()
-
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
-  });
+  window.spotifyReady = true
 }
-
-
+console.log('tekjhsdas')
