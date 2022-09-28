6ed41ce723125d59f7b6ca71c8e6836b31f9e49f
bryan <hi@braynm.com>
2022-09-28 19:59:53 +0800

    clean up js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-//import "../css/app.css"
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
+//// If you want to use Phoenix channels, run `mix help phx.gen.channel`
 
 // to get started and then uncomment the line below.
 // import "./user_socket.js"
@@ -25,107 +24,18 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 
-import "mdn-polyfills/Object.assign"
-import "mdn-polyfills/CustomEvent"
-import "mdn-polyfills/String.prototype.startsWith"
-import "mdn-polyfills/Array.from"
-import "mdn-polyfills/Array.prototype.find"
-import "mdn-polyfills/Array.prototype.some"
-import "mdn-polyfills/NodeList.prototype.forEach"
-import "mdn-polyfills/Element.prototype.closest"
-import "mdn-polyfills/Element.prototype.matches"
-import "mdn-polyfills/Node.prototype.remove"
-import "child-replace-with-polyfill"
-import "url-search-params-polyfill"
-import "formdata-polyfill"
-import "classlist-polyfill"
-import "new-event-polyfill"
-import "@webcomponents/template"
-import "shim-keyboard-event-key"
-import "core-js/features/set"
-import "core-js/features/url"
-
+import Playback from "./Playback"
+import TrackPagination from "./TrackPagination"
+import PlaylistPagination from "./PlaylistPagination"
 import topbar from "../vendor/topbar"
 
 window.spotifyReady = false
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let isAllLoaded = false
 let hooks = {
-  Playback2312: {
-    deviceId: null,
-    player: null,
-    initializePlayer() {
-      this.player = new Spotify.Player({
-        name: "ExPotify",
-        getOAuthToken: (cb) => {
-          cb(this.token)
-        }
-      })
-
-      window.addEventListener("keyup", (event) => {
-        switch (event.keyCode) {
-          case 32:
-            this.player.togglePlay()
-          default:
-            console.log("something else..")
-        }
-      })
-
-      this.player.addListener("ready", ({ device_id }) => {
-        this.deviceId = device_id
-        this.pushEvent("init_device", { device_id })
-      })
-
-      this.player.addListener('initialization_error', ({ message }) => {
-        console.error(message);
-      })
-
-      this.player.addListener('authentication_error', ({ message }) => {
-        console.error(message);
-      })
-
-      this.player.addListener('account_error', ({ message }) => {
-        console.error(message);
-      })
-
-      this.player.addListener('playback_error', ({ message }) => {
-        console.error(message);
-      })
-    },
-    mounted() {
-      this.handleEvent("init_token", ({ token }) => {
-        this.token = token
-        if (window.spotifyReady) {
-            console.log('222313512316')
-            this.initializePlayer()
-            this.player.connect()
-        }
-      })
-    },
-    updated() {
-
-    }
-  },
-  Scroll: {
-    mounted() {
-      this.observer = new IntersectionObserver(
-        ([entry]) => {
-          if (entry.isIntersecting && isAllLoaded === false) {
-            this.pushEvent("load_more", {})
-          }
-        },
-        { rootMargin: "0px", threshold: 1.0, }
-      );
-
-      this.observer.observe(this.el);
-      this.handleEvent("all_loaded", data => {
-        isAllLoaded = true
-      })
-    },
-    beforeDestroy() {
-      this.observer.unobserve(this.el)
-    }
-  },
+  Playback,
+  TrackPagination,
+  PlaylistPagination
 }
 
 let liveSocket = new LiveSocket("/live", Socket, {hooks, params: {_csrf_token: csrfToken}})
@@ -136,7 +46,7 @@ window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 window.addEventListener("phx:page-loading-start", () => {
-  if(!topBarScheduled) {
+  if (!topBarScheduled) {
     topBarScheduled = setTimeout(() => topbar.show(), 200)
   }
 })
@@ -158,4 +68,3 @@ window.liveSocket = liveSocket
 window.onSpotifyWebPlaybackSDKReady = () => {
   window.spotifyReady = true
 }
-console.log('tekjhsdas')
