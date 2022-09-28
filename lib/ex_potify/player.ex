867098cfb0101d9869e6f64522122cc130620d6a
bryan <hi@braynm.com>
2022-09-28 19:59:59 +0800

    spotify api use case

--- lib/ex_potify/player.ex
+++ lib/ex_potify/player.ex
@@ -1,13 +1,30 @@
 defmodule ExPotify.Player do
-  def play(user_id, {device_id, uri}) do
+  def play(user_id, {device_id, payload}) do
     access_token = ExPotify.TokenRefresher.get_access_token(user_id)
     credentials = %Spotify.Credentials{access_token: access_token}
 
     params = [
       device_id: device_id,
-      uris: uri
+      context_uri: payload[:context_uri],
+      offset: %{
+        position: payload[:offset]
+      }
     ]
 
     Spotify.Player.play(credentials, params)
+  end
+
+  def skip_to_next(user_id) do
+    access_token = ExPotify.TokenRefresher.get_access_token(user_id)
+    credentials = %Spotify.Credentials{access_token: access_token}
+
+    Spotify.Player.skip_to_next(credentials)
+  end
+
+  def skip_to_previous(user_id) do
+    access_token = ExPotify.TokenRefresher.get_access_token(user_id)
+    credentials = %Spotify.Credentials{access_token: access_token}
+
+    Spotify.Player.skip_to_previous(credentials)
   end
 end
